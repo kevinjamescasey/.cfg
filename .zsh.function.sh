38b9ded51b446c018b043488c65603a14dad8f6a
Kevin Casey <kevin.casey@centene.com>
2026-05-06 15:20:06 -0500

    zsh functions

--- .zsh.function.sh
+++ .zsh.function.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
 
+# from https://gist.github.com/kevinjamescasey/8e1458ee0cb0ff78a96056713c4c339f
 function cdgitroot() {
 
 cd `git rev-parse --show-toplevel`
 
+}
+
+function awsid() {
+    aws sts get-caller-identity
+    aws iam list-account-aliases 
 }
