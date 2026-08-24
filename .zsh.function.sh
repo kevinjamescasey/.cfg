33cec3366eb3a5d6f43730f146a723b2555e5cc2
Kevin Casey <kevin.casey@centene.com>
2026-08-24 09:29:26 -0500

    awsid function

--- .zsh.function.sh
+++ .zsh.function.sh
@@ -2,12 +2,19 @@
 
 # from https://gist.github.com/kevinjamescasey/8e1458ee0cb0ff78a96056713c4c339f
 function cdgitroot() {
-
-cd `git rev-parse --show-toplevel`
-
+    cd `git rev-parse --show-toplevel`
 }
 
 function awsid() {
     aws sts get-caller-identity
     aws iam list-account-aliases 
 }
+
+function awsIamErrorDecode() {
+    # need aws and jq installed
+    # need to be logged in
+    local encoded_message="$1"
+    decoded_message=$(AWS_REGION=us-east-1 aws sts decode-authorization-message --encoded-message "$encoded_message")
+    echo $decoded_message|jq -r .DecodedMessage |sed 's/\\"/"/g'|jq
+}
+
