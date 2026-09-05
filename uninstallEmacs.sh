bfa03c065969a7af7c643a206f43fd5fef7b12eb
Kevin Casey <12061237+kevinjamescasey@users.noreply.github.com>
2026-09-05 12:51:09 -0500

    emacs unintstall

--- uninstallEmacs.sh
+++ uninstallEmacs.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env sh -x
 
-rm -rf ~/Applications/Emacs.app/
+rm -rf ~/Applications/Emacs.app/ # mac GUI launcher
 
-rm -rf ~/.emacs.d.doom
+rm -rf ~/.emacs.d.doom # old Doom location
+rm -rf ~/.config/emacs # Doom location
 
-brew uninstall git ripgrep coreutils fd libvterm emacs-mac shellcheck
+brew uninstall git ripgrep coreutils fd libvterm emacs-mac shellcheck fontconfig shfmt
+brew uninstall --cask font-symbols-only-nerd-font
 
-# npm unisntall -g prettier marked
+npm unisntall -g prettier marked
 
-rm -rf ~/.emacs.d
-rm -rf ~/.emacs.d.gnu
+rm -rf ~/.emacs.d     #one of the defaul GNU Emacs config locations where I keep Chemacs
+rm -rf ~/.emacs.d.gnu # location where I tend to backup the default config that GNU Emacs generates
