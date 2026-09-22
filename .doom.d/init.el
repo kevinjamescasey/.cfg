65090c2c33299e5c9f39cfd73d45a34fbf59924a
Kevin Casey <kevin.casey@centene.com>
2026-09-22 09:39:48 -0500

    fix doom dashboard

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -29,7 +29,7 @@
        :ui
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
-       dashboard    ; a nifty splash screen for Emacs
+       doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;(emoji +unicode)  ; 🙂
        ;;fill-column       ; a `fill-column' indicator
